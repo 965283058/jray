581ee2430959d186de4dc770f0ec2ccde7fbf417
jiangpeng <jiangpeng@houbank.com>
2017-11-30 15:53:29 +0800

    修复Bug

--- main/index.js
+++ main/index.js
@@ -1,13 +1,26 @@
+
 const JRay = require("../lib/application")
 let app = new JRay()
-app.use(function (ctx, next) {
+app.use(async function (ctx, next) {
     ctx.body = "hello"
-    next()
+    try {
+        await next()
+    } catch (e) {
+        ctx.body = e.message
+        console.info("cathc run")
+    }
 })
 
-app.use(function (ctx, next) {
+app.use(async function (ctx, next) {
     ctx.body += " 呵呵"
-    next()
+    await next()
+})
+
+app.use(async(ctx, next)=> {
+    ctx.body += " mmd"
+    throw new Error("i love you ")
+    await next()
 })
 
 app.listen(3000)
+
