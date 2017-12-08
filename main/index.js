ec5c6565cb6536f1951800ae03992406c6901354
jiangpeng <jiangpeng@houbank.com>
2017-12-08 11:25:08 +0800

    添加static 、router

--- main/index.js
+++ main/index.js
@@ -1,30 +1,40 @@
 const JRay = require("../lib/application")
+const bodyParse = require("../lib/bodyParse")
+const static = require("../lib/static")
+const path = require("path")
+let Router = require("../lib/router")
 let app = new JRay()
+let appRouter = new Router()
+
+
 app.use(async function (ctx, next) {
-    ctx.body = "hello"
     try {
-        await sellp(3000)
         await next()
     } catch (e) {
         ctx.body = e.message
     }
 })
-
-app.use(async function (ctx, next) {
-    ctx.body += " 呵呵"
+app.use(static({
+    root: path.resolve(path.join(process.cwd(), "../static"))
+}))
+app.use(bodyParse)
+app.use(appRouter.routes())
+appRouter.get("/test", async(ctx, next)=> {
+    ctx.body = {"test": 123456}
     await next()
 })
-
-app.use(async(ctx, next)=> {
-    ctx.body += " mmd"
-   // throw new Error("not bug")
+appRouter.get("/test/name", async(ctx, next)=> {
+    ctx.body = {"test": 1234567890}
+    await next()
+})
+appRouter.get("/demo/name", async(ctx, next)=> {
+    ctx.body = {"test": "demo"}
     await next()
 })
 
 app.listen(3000)
 
-
-let sellp = async(i)=> {
+let seelp = async(i)=> {
     return new Promise((rev, rej)=> {
         setTimeout(rev, i)
     })
